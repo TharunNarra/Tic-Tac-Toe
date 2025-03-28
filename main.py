d9dc28b2860768e74e8cfd68caaba29b4d1368f9
TharunNarra <157900739+TharunNarra@users.noreply.github.com>
2025-03-28 22:37:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.secret_key = '8gdQTbFaIK'  # Change this to a secure secret key
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Tharun@2003',
+    'password': 'MYSql_Password',#place your password
     'database': 'game_db'
 }
 
